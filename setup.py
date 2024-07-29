104815baecfeec5ca39d51060b6a45acc4ca45fa
Kostya Tatoshin <kostyatatoshin@gmail.com>
2024-07-29 14:04:32 +0300

    Fix setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ import os
 
 os.system("python -m venv venv")
 if os.name == "nt":
-    os.system(os.path.join("venv", "Scripts", "pip.exe") + " install -r requirements")
+    os.system(os.path.join("venv", "Scripts", "pip.exe") + " install -r requirements.txt")
 else:
     os.system(os.path.join("venv", "bin", "pip") + " install -r requirements.txt")
